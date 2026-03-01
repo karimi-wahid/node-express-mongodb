1eb48e44552efb15546479c18ee13720e5169e4f
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-01 21:23:46 +0430

    CRUD of tour

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+console.log(process.env.DATABASE_LOCAL);
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log('DB connected!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
