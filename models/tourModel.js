81628743b5c098afd529066cbca07de72f377d8a
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-10 09:14:49 +0430

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -96,6 +96,13 @@ tourSchema.post(/^find/, function(docs, next) {
   next();
 });
 
+// Aggregation Middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this);
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
