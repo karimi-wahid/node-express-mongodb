bc79db4274a010d687ab9ebbf8f37b7ef12b887b
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-14 11:02:05 +0430

    handling duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,6 +9,12 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -50,6 +56,7 @@ module.exports = (err, req, res, next) => {
     error.name = err.name;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
