60a26c683b497aa13c1478be74dd3794847ab082
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-18 10:36:24 +0430

    Errors outisde express Unhandled Rejections

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ mongoose
   .then(() => console.log('DB connected!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandler Rejection Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
