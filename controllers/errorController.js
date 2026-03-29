f8a567eaa2ff81385060ce31299ed5840a04b306
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-29 07:26:32 +0430

    Protected Routes for (Tour)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -22,6 +22,12 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -67,5 +73,7 @@ module.exports = (err, req, res, next) => {
     sendErrorProd(error, res);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
   }
 };
