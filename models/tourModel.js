cc0d243a4455def7bfa7e110c5925f7f16b60eb9
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-04-08 07:03:06 +0430

    Modelling Tour Guides Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -101,7 +102,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -118,6 +120,11 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
 
   next();
+});
+
+tourSchema.pre('save', async function() {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
 });
 
 // tourSchema.post('save', function(doc, next) {
